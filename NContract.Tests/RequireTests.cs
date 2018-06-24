b7c37cb92db524397e6718749173f89a3d376f49
Niklas Hedlund <niklas@orisoft.com>
2018-06-24 20:20:10 +0200

    Add NotNull

--- NContract.Tests/RequireTests.cs
+++ NContract.Tests/RequireTests.cs
@@ -1,6 +1,20 @@
+using System;
+using Xunit;
+
 namespace NContract.Tests
 {
     public class RequireTests
     {
+        [Fact]
+        public void NotNull_WhenNull_ThrowsArgumentNullException()
+        {
+            Assert.Throws<ArgumentNullException>(() => Require.NotNull(null, "name"));
+        }
+
+        [Fact]
+        public void NotNull_WhenNotNull_DoesNotThrow()
+        {
+            Require.NotNull("", "name");
+        }
     }
 }
--- NContract/Require.cs
+++ NContract/Require.cs
@@ -1,6 +1,15 @@
+using System;
+using System.Diagnostics;
+
 namespace NContract
 {
+    [DebuggerStepThrough]
     public static class Require
     {
+        public static void NotNull(object value, string parameterName)
+        {
+            if (value == null)
+                throw new ArgumentNullException(parameterName);
+        }
     }
 }
